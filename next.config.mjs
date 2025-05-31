bfc1efd7e0eb79619c5594492ab5b432469a6339
Sanjay <attellisanjay29@gmail.com>
2025-05-31 13:00:01 +0530

    first commit

---
+++ next.config.mjs
@@ -0,0 +1,12 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  images: {
+    domains: [
+      'utfs.io',
+      'subdomain',
+    ],
+  },
+  reactStrictMode: false,
+};
+
+export default nextConfig;
